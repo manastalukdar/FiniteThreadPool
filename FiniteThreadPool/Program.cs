029bb90f1e1f0620b212b294c7542b34cd2d9233
Manas Talukdar <manas.talukdar@gmail.com>
2016-04-04 00:35:28 -0700

    work in progress

--- FiniteThreadPool/Program.cs
+++ FiniteThreadPool/Program.cs
@@ -17,9 +17,10 @@ namespace FiniteThreadPool
         static void Main(string[] args)
         {
             _semaphore = new SemaphoreSlim(MAX_PARALLEL_UPLOADS);
-            for (int _jobCount = 0; _jobCount < 10; _jobCount++)
+            for (int _jobCount = 0; _jobCount < 1; _jobCount++)
             {
-                Console.WriteLine("Datetime: {0}, _jobCount: {1}", (DateTime.Now - _start).TotalMilliseconds, _jobCount);
+                Console.WriteLine("Datetime_Main: {0}, _jobCount: {1}, semaphore: {2}", (DateTime.Now - _start).TotalMilliseconds, _jobCount, _semaphore.CurrentCount);
+                _semaphore.WaitAsync().Wait();
                 DoJob(_jobCount);
             }
 
@@ -28,28 +29,21 @@ namespace FiniteThreadPool
 
         static async Task DoJob(int jobCount)
         {
-            Console.WriteLine("Datetime: {0}, jobCount: {1}, _semaphore: {2}", (DateTime.Now - _start).TotalMilliseconds, jobCount, _semaphore.CurrentCount);
-            await Task.Factory.StartNew(async () =>
-            {
-                await _semaphore.WaitAsync();
-                try
-                {
-                    Dummy(jobCount);
-                    Console.WriteLine("Datetime: {0}, jobCount: {1}, _semaphore: {2} after Dummy", (DateTime.Now - _start).TotalMilliseconds, jobCount, _semaphore.CurrentCount);
-                }
-                finally
-                {
-                    //_semaphore.Release();
-                    Console.WriteLine("Datetime: {0}, jobCount: {1}, _semaphore: {2} after Release", (DateTime.Now - _start).TotalMilliseconds, jobCount, _semaphore.CurrentCount);
-                }
-            });
+            Console.WriteLine("Datetime_DoJob: {0}, jobCount: {1}, _semaphore: {2}", (DateTime.Now - _start).TotalMilliseconds, jobCount, _semaphore.CurrentCount);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(30000);
+            await Task.Factory.StartNew(() => Dummy(jobCount, cts));
+            _semaphore.Release();
+            Console.WriteLine("Datetime_Release: {0}, jobCount: {1}, semaphore: {2} after Release", (DateTime.Now - _start).TotalMilliseconds, jobCount, _semaphore.CurrentCount);
         }
 
-        static async Task Dummy(int jobCount)
+        static async Task Dummy(int jobCount, CancellationTokenSource cts)
         {
-            await Task.Delay(3000);
-            _semaphore.Release();
-            Console.WriteLine("Datetime: {0}, jobCount: {1}", (DateTime.Now - _start).TotalMilliseconds, jobCount);
+            await Task.Delay(5000);
+            if (cts.IsCancellationRequested)
+            {
+                Console.WriteLine("dummy");
+            }
         }
     }
 }
